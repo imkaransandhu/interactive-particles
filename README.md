19da42f197f0499dc46b5f90d8c3afa50b398f6d
Karan Singh <70357137+imkaransandhu@users.noreply.github.com>
2023-02-27 11:33:00 +1300

    Upd: Intergrated Interactive particle library in Next.js App

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Interactive Wall
-
-# karan branch from develop
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  webpack: (config) => {
+    // Add GSLX loader for *.vert and *.frag files
+    config.module.rules.push({
+      test: /\.(vert|frag)$/,
+      use: ["glslify-import-loader", "raw-loader", "glslify-loader"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
